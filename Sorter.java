91c4ffa8e7accbf1d10325e9a2f4c5784b027729
عبدالرحمن الحيدري <s202164970@gmail.com>
2024-02-05 16:34:38 +0300

    add function

---
+++ Sorter.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Sorter {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+
+
+    }
+   public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the element at index i
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
